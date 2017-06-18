a471f8aa0df0ee62dde40579b6bad23873c6ff06
Adam Charron <adam@charrondev.com>
2017-06-18 12:53:22 -0400

    Complete reset of build process

--- src/Vanilla/FrontendTools/versions/1.0/build.stylesheets.js
+++ src/Vanilla/FrontendTools/versions/1.0/build.stylesheets.js
@@ -3,24 +3,28 @@ const path = require('path');
 
 const plumber = require('gulp-plumber');
 const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('gulp-cssnano');
 const stylelint = require('gulp-stylelint');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const size = require('gulp-size');
 
 module.exports = (addonDirectory) => {
+    const destination = path.resolve(addonDirectory, 'design');
+
     return gulp
         .src(path.resolve(addonDirectory, 'src/scss/*.scss'))
         .pipe(plumber())
         .pipe(sourcemaps.init())
-        .pipe(
-            stylelint({
-                reporters: [{ formatter: 'string', console: true }]
-            })
-        )
+        // .pipe(
+        //     stylelint({
+        //         reporters: [{ formatter: 'string', console: true }]
+        //     })
+        // )
+        .pipe(cssnano())
         .pipe(sass())
         .pipe(autoprefixer())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.resolve(addonDirectory, 'design')))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(destination))
         .pipe(size({ showFiles: true }));
 }
--- src/Vanilla/FrontendTools/versions/1.0/gulpfile.js
+++ src/Vanilla/FrontendTools/versions/1.0/gulpfile.js
@@ -1,49 +1,52 @@
-const gulp = require('gulp');
-const argv = require('yargs').argv;
+const path = require("path");
+const gulp = require("gulp");
+const livereload = require("gulp-livereload");
+const argv = require("yargs").argv;
 
-const VanillUtility = require('../../VanillaUtility');
+const VanillUtility = require("../../VanillaUtility");
 
-const buildJs = require('./build.javascript');
-const buildStyles = require('./build.stylesheets');
-const buildAssets = require('./build.assets');
+const buildJs = require("./build.javascript");
+const buildStyles = require("./build.stylesheets");
+const buildAssets = require("./build.assets");
 
 const addonpath = argv.addonpath;
 
-gulp.task('build:js', () => {
-    return VanillUtility.getJsEntries(addonpath)
-        .then((jsfiles) => {
-            return buildJs(addonpath, jsfiles);
-        });
+gulp.task("build:js", () => {
+    return VanillUtility.getJsEntries(addonpath).then(jsfiles => {
+        return buildJs(addonpath, jsfiles);
+    });
 });
 
-gulp.task('build:styles', () => {
+gulp.task("build:styles", () => {
     return buildStyles(addonpath);
-})
-
-gulp.task('build:assets', () => {
-    return buildAssets(addonpath);
-})
-
-gulp.task('default', () => {
-    gulp.start('build:styles', 'build:js', 'build:assets');
 });
 
-gulp.task('watch', ['default'], function () {
+gulp.task("build:assets", () => {
+    return buildAssets(addonpath);
+});
+
+gulp.task("build", ["build:js", "build:assets", "build:styles"]);
+
+gulp.task("default", ["build"], () => {});
+
+gulp.task("watch", ["default"], function() {
     livereload.listen();
 
-    gulp.watch([
-            path.resolve(addonpath, 'design/*.css'),
-            path.resolve(addonpath, 'design/images/**/*'),
-            path.resolve(addonpath, 'js/*.js'),
-            path.resolve(addonpath, 'views/**/*')
-        ], (file) => {
+    gulp.watch(
+        [
+            path.resolve(addonpath, "design/*.css"),
+            path.resolve(addonpath, "design/images/**/*"),
+            path.resolve(addonpath, "js/*.js"),
+            path.resolve(addonpath, "views/**/*")
+        ],
+        file => {
             return livereload.changed(file.path);
         }
     );
 
-    gulp.watch(path.resolve(addonpath, 'src/scss/**/*.scss'), ['build:styles']);
-    gulp.watch(path.resolve(addonpath, 'src/js/**/*.js'), ['build:js']);
-    gulp.watch(path.resolve(addonpath, 'design/images/**/*'), ['build:assets']);
+    gulp.watch(path.resolve(addonpath, "src/scss/**/*.scss"), ["build:styles"]);
+    gulp.watch(path.resolve(addonpath, "src/js/**/*.js"), ["build:js"]);
+    gulp.watch(path.resolve(addonpath, "design/images/**/*"), ["build:assets"]);
 });
 
 module.exports = gulp;
